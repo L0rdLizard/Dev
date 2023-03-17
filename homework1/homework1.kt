cc918126df553b47434d46e574743cebc2de3c23
LordLizard <WarriorXKz@gmail.com>
2023-03-17 18:43:05 +0300

    Make BC ages

--- homework1/homework1.kt
+++ homework1/homework1.kt
@@ -1,10 +1,16 @@
 package homework1
 
-fun century (x : Int) : Int{
-    return x / 100 + 1
+fun century (x : Int){
+    if (x < 0) {
+        val y = x * -1
+        if (x % 100 == 0) println("$x is " + (y / 100) + " BC")
+        else println("$x is " + (y / 100 + 1) + " BC")
+    }else {
+        if (x % 100 == 0) println("$x is " + (x / 100) + " century")
+        else println("$x is " + (x / 100 + 1) + " century")
+    }
 }
 fun main () {
-    val year = 1821
-    val c = century(year)
-    println("Century of $year is $c")
+    val year = -1654
+    century(year)
 }
