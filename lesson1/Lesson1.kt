540c1cfe068d015adfa9945db2751cf6e15aa712
LordLizard <WarriorXKz@gmail.com>
2023-03-17 15:38:43 +0300

    lesson1

---
+++ lesson1/Lesson1.kt
@@ -0,0 +1,14 @@
+package lesson1
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+    val year = 2000
+    if ((year % 4 == 0) || (year % 400 == 0)) {
+        if ((year % 100 == 0) && (year % 400 != 0)) {
+            println("Not leap year")
+            return
+        }
+        println("Leap year")
+    }else println("Not leap year")
+}
