0d2f5515edf04823f23fc06d52e61dec183669f3
LordLizard <WarriorXKz@gmail.com>
2023-03-21 22:10:29 +0300

    changes from lesson3

--- code/homework2/n1/n1.kt
+++ code/homework2/n1/n1.kt
@@ -1,5 +1,6 @@
 package code.homework2.n1
 
+//(1 until number).filter { it % 3 == 0 || it % 5 == 0 }.sum()
 fun threefive(x : Int) : Int{
     if (x <= 0) return 0
     var sum = 0
--- code/homework2/n2/n2.kt
+++ code/homework2/n2/n2.kt
@@ -2,24 +2,41 @@ package code.homework2.n2
 
 import kotlin.text.StringBuilder
 
+//fun capit(stroka : String) : String{
+//    val result = StringBuilder()
+//    var flag = 0
+//    for (element in stroka.indices){
+//        if (flag == 1) {
+//            result.append(stroka[element].uppercaseChar())
+//            flag = 0
+//            continue
+//        }
+//        if ((stroka[element] == '-') || (stroka[element] == '_')){
+//            flag = 1
+//            continue
+//        }
+//        result.append(stroka[element])
+//    }
+//    return result.toString()
+//}
+
 fun capit(stroka : String) : String{
     val result = StringBuilder()
-    var flag = 0
-    for (element in stroka.indices){
-        if (flag == 1) {
-            result.append(stroka[element].uppercaseChar())
-            flag = 0
+    var flag : Boolean = false
+    for (element in stroka){
+        if (flag) {
+            result.append(element.uppercaseChar())
+            flag = false
             continue
         }
-        if ((stroka[element] == '-') || (stroka[element] == '_')){
-            flag = 1
+        if ((element == '-') || (element == '_')){
+            flag = true
             continue
         }
-        result.append(stroka[element])
+        result.append(element)
     }
     return result.toString()
 }
-
 
 fun main(){
     var teststroka = "The_Stealth-Warrior"
